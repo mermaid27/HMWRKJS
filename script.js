1c4228eeeb2b2707fb9fd21d0b88dc5e0269eabb
mermaid27 <n.ghurbela@gmail.com>
2022-11-05 18:40:30 +0000

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+let firstname = "giorgi"
+    
+let lastname = "smith"
+let studentage = 25 
+let studentstatus = "active" 
+
+
+console.log (studentstatus);  
+
+
+let studentnames = ["ani", "mari", "ilia", "sandro", "khatia", "inga", "rezo"]; 
+
+console.log (studentnames [studentnames -2]); 
+
+
+
+
+
+
