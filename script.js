2ffd7e5f8a15f11940783e1e1740202d2d9cb8af
mermaid27 <n.ghurbela@gmail.com>
2022-11-06 19:51:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,16 @@ for (let y of array) {
 
     }
 }
+11. 
 
+let number =[1,2,3,4,5,6,7,8,9,10];
+for (let item of number) {
+    if (item == 5) {
+        break; 
+
+    }
+    console.log(item); 
+}
 
 
 
